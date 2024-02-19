c8733bf7ab03220a80d1ce837d2945e5692e04f8
Edvando Simplicio <edvandosimplicio0508@gmail.com>
2024-02-18 21:37:33 -0300

    2. adição dos scripts poke-api e do nosso objeto pokemon-model

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
-console.log('Sucesso!')
+/*--transformando pokemons em Lists HTML-->*/
+function convertPokemonToLi(pokemon){
+    return `
+    
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+
+            </div>
+
+        </li>
+
+    `
+}
+
+//adicionando nossa estrutura de lista html por id em uma constante
+const pokemonList = document.getElementById('pokemonList')
+
+
+    // 1. pokeApi é um objeto que possui o método .getPokemons() com uma arrowfunction com todo o código de requisição http
+    // 2. .then() é o último método padrão que com arrowfunction retorna todo os elementos dos pokemons convertido e filtrado
+    pokeApi.getPokemons().then((pokemons = []) => {
+
+        //adicionando em uma const o  mapeamento(repetição) dos pokemons pelo tamanho limitado na url(10 vezes) - convertendo em html e juntando todos sem vírgula.
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+
+        //adiciona (em formato html) todo o processo acima na nossa atual lista de pokemon.
+        pokemonList.innerHTML = newHtml
+    })
---
+++ assets/js/poke-api.js
@@ -0,0 +1,42 @@
+const pokeApi = {}
+
+//converte todos os detalhes dos pokemons para o nosso objeto Pokemon no arquivo pokemon-model.js
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+//requisição http para buscar todos os detalhes dos pokemons e passa para a nossa function acima que converte o que nos interessa pro nosso objeto Pokemon
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then (convertPokeApiDetailToPokemon)
+}
+
+//criação do método com url da api settada em 10 pokemons
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    //requisição http para buscar os pokemons
+    return fetch(url)
+
+        //transforma em json
+        .then((response) => response.json())
+        //filtra apenas os resultados dos 10 pokemons limitados que escolhemos
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+
+}
