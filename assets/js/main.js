0f79f267278b7d7a46e202c54098ca309486209b
Edvando Simplicio <edvandosimplicio0508@gmail.com>
2024-02-18 22:26:05 -0300

    4. Adição do código script do bottom de paginação e exclusão da estrutura estática HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
-/*--transformando pokemons em Lists HTML-->*/
-function convertPokemonToLi(pokemon){
-    return `
+//adicionando nossa estrutura de lista html por id em uma constante
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0;
+
+function loadPokemonItens(offset, limit) {
+    // 1. pokeApi é um objeto que possui o método .getPokemons() com uma arrowfunction com todo o código de requisição http
+    // 2. .then() é o último método padrão que com arrowfunction retorna todo os elementos dos pokemons convertido e filtrado
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+
+        //adicionando em uma const o  mapeamento(repetição) dos pokemons pelo tamanho limitado na url(10 vezes) - convertendo em html e juntando todos sem vírgula.
+        /*--transformando pokemons em Lists HTML-->*/
+        const newHtml = pokemons.map((pokemon) => `
     
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
@@ -18,20 +29,16 @@ function convertPokemonToLi(pokemon){
 
         </li>
 
-    `
-}
-
-//adicionando nossa estrutura de lista html por id em uma constante
-const pokemonList = document.getElementById('pokemonList')
-
-
-    // 1. pokeApi é um objeto que possui o método .getPokemons() com uma arrowfunction com todo o código de requisição http
-    // 2. .then() é o último método padrão que com arrowfunction retorna todo os elementos dos pokemons convertido e filtrado
-    pokeApi.getPokemons().then((pokemons = []) => {
-
-        //adicionando em uma const o  mapeamento(repetição) dos pokemons pelo tamanho limitado na url(10 vezes) - convertendo em html e juntando todos sem vírgula.
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
+    ` ).join('')
 
         //adiciona (em formato html) todo o processo acima na nossa atual lista de pokemon.
-        pokemonList.innerHTML = newHtml
+        pokemonList.innerHTML += newHtml
     })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
